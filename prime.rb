3bc21bdf6150a62002bac51b787d6a19bd1e38b6
Raymond Chow <raychow1944@yahoo.com>
2018-12-20 00:18:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(n)
+  result = false
+  if n == 2 
+    result = true
+  end
+  if n <= 1
+    result = false
+  end
+  
+  for i in 2...n do
+    if n % i == 0
+      return false
+    else
+      result = true
+    end
+  end
+  result
+end
